c89e3b98b88ad36baaebaf036073dca5fc031e4f
Bob Groeneveld <bob.groeneveld@gmail.com>
2014-08-07 23:16:03 +0200

    minor fixes

--- javascript/main.js
+++ javascript/main.js
@@ -6,10 +6,12 @@ document.addEventListener('DOMContentLoaded', function(){
   // var tmp = setTimeout( function() { loadPlayer(); }, 2000);
   chrome.storage.local.get('player_id',function(x){
     var d = new Date();
-    d.setTime(d.getTime() + (20*365*24*60*60*1000));
+    d.setTime(d.getTime() + (20*365*24*60*60*1000)); // set 20 years into the future
     var expires = "expires="+d.toGMTString();
-    document.cookie = 'player_id' + "=" + x["url"] + "; " + expires;
-    // document.getElementById("browser").attr('src',x["url"]).get(0).reload();
-    document.getElementById("browser").setAttribute('src', 'http://play.playr.biz/');
+    document.cookie = 'player_id' + "=" + x["player_id"] + "; " + expires;
+    //document.getElementById("browser").attr('src',x["url"]).get(0).reload();
+    document.getElementById("browser").setAttribute('style', 'width:' + window.innerWidth + 'px;height:'+window.innerHeight+'px;');
+    // document.getElementById("browser").setAttribute('src', 'http://www.google.com/').reload();
+    document.getElementById("browser").setAttribute('src', 'http://play.playr.biz/').reload();
   });
  });
