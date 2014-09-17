4be3224ec2244a20607d75b3af590513b74d8a42
Bob Groeneveld <bob.groeneveld@gmail.com>
2014-09-17 18:43:19 +0200

    add more info on screen and log app update

--- javascript/background.js
+++ javascript/background.js
@@ -1,22 +1,24 @@
+var operatingSystem;
+
 getUUID = function() {
   "use strict"
   // generate a type 4 (random) UUID
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {var r = Math.random()*16|0,v=c=='x'?r:r&0x3|0x8;return v.toString(16);});
 }
 
-chrome.app.runtime.onLaunched.addListener(init);
-chrome.app.runtime.onRestarted.addListener(init);
 
 function init() {
   "use strict"
   //don't let computer sleep
   chrome.power.requestKeepAwake("display");
 
+  determineOperatingSystem();
   chrome.storage.local.get('player_id',function(content){
     if(('player_id' in content)){
       // player_id defined no action required
     }else{
       chrome.storage.local.set({'player_id':getUUID()});
+      console.log("player_id was not yet defined, starting reload...");
       chrome.runtime.reload();
     }
     openWindow("main.html");
@@ -35,7 +37,34 @@ function init() {
         }
       },function(window){
         window.fullscreen();
+        window.onClosed.addListener(windowOnClosed);
       });
     });
   }
-}
+};
+
+function windowOnClosed() {
+  "use strict"
+	console.log("window.Onclosed received");
+	chrome.power.releaseKeepAwake();
+};
+
+function determineOperatingSystem() {
+  "use strict"
+  chrome.runtime.getPlatformInfo(function(platformInformation) {
+		operatingSystem = platformInformation.os;
+    console.log("Operating System: " + operatingSystem);
+  });
+};
+
+chrome.runtime.onUpdateAvailable.addListener(function(details) {
+	if (operatingSystem === "cros") {
+		console.log("Update available to version: " + details.version + ". Starting update.");
+		chrome.runtime.reload();
+	} else {
+		console.log("onUpdateAvailable received but not supported on " + operatingSystem);
+  }
+});
+
+chrome.app.runtime.onLaunched.addListener(init);
+chrome.app.runtime.onRestarted.addListener(init);
